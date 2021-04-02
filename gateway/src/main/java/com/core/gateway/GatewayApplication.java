f17181d9d9070449445f11a6b1ed0bb4e0394490
Antispiner <trevel007@mail.ru>
2021-04-02 18:29:40 +0300

    microservices - test - hystrix

--- gateway/src/main/java/com/core/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/core/gateway/GatewayApplication.java
@@ -2,10 +2,14 @@ package com.core.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @EnableZuulProxy
 public class GatewayApplication {
--- turbine-stream-service/src/main/java/com/core/turbine/TurbineStreamServiceApplication.java
+++ turbine-stream-service/src/main/java/com/core/turbine/TurbineStreamServiceApplication.java
@@ -2,12 +2,10 @@ package com.core.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
 @EnableTurbineStream
-@EnableDiscoveryClient
 public class TurbineStreamServiceApplication {
 
 	public static void main(String[] args) {
