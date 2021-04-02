2976fd47671c2d7c38d12fcd70783035f7173fca
Antispiner <trevel007@mail.ru>
2021-04-02 18:58:34 +0300

    microservices - test - metrics

--- turbine-stream-service/src/main/java/com/core/turbine/TurbineStreamServiceApplication.java
+++ turbine-stream-service/src/main/java/com/core/turbine/TurbineStreamServiceApplication.java
@@ -2,10 +2,10 @@ package com.core.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableTurbineStream
+@EnableDiscoveryClient
 public class TurbineStreamServiceApplication {
 
 	public static void main(String[] args) {
